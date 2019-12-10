026fa9b9d9728aad5826c381a1ca9ae592e79b38
Sepehr Sadri <sepehrsadri@gmail.com>
2019-12-11 00:20:35 +0330

    Add activityLifecycleCallbacks listener

--- app/src/main/java/sepehr/project/com/androidoreoserviceexample/BaseApplication.kt
+++ app/src/main/java/sepehr/project/com/androidoreoserviceexample/BaseApplication.kt
@@ -1,5 +1,7 @@
 package sepehr.project.com.androidoreoserviceexample
 
+import android.app.Activity
+import android.os.Bundle
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import sepehr.project.com.androidoreoserviceexample.di.app.DaggerAppComponent
@@ -7,11 +9,46 @@ import timber.log.Timber
 
 class BaseApplication : DaggerApplication() {
 
+    private val activityLifecycleCallbacks =
+        object : ActivityLifecycleCallbacks {
+            override fun onActivityPaused(activity: Activity) {
+                //ignore
+            }
+
+            override fun onActivityStarted(activity: Activity) {
+                //ignore
+            }
+
+            override fun onActivityDestroyed(activity: Activity) {
+                //ignore
+            }
+
+            override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+                //ignore
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                //ignore
+            }
+
+            override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+                TODO("Start service !")
+            }
+
+            override fun onActivityResumed(activity: Activity) {
+                //ignore
+            }
+        }
+
 
     override fun onCreate() {
         super.onCreate()
 
         initTimber()
+
+        registerActivityLifecycleCallbacks(
+            activityLifecycleCallbacks
+        )
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
